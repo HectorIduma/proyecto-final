02fac0e180ea7f478a0f9bd4de016e4b8dad5c72
Genar0Islas <164415634+Genar0Islas@users.noreply.github.com>
2025-05-11 21:10:42 -0700

    Add files via upload

--- revista_clases.py
+++ revista_clases.py
@@ -57,6 +57,9 @@ class sistema_de_busqueda:
             if revista.title.strip().lower().startswith(letra_buscada.lower()):
                 revistas_encontradas.append(revista.to_dict())
         return revistas_encontradas
+    
+    def buscar_revistas_por_titulo(titulo_parcial):
+        return [revista for revista in revista if titulo_parcial.lower() in revista.title.lower()]
 
 
     
